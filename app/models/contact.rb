0f1c04f2cdfe94b743683525297b4c9fdef33ea9
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-02-07 21:57:15 +0000

    finsihed contact_book

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,6 @@
  _Rails will automatically require that class in every other file in our app.
 _ Notice that we named the class singularly (Contact), rather than plurally (Contacts). This is because each instance of this class is going to represent one row from the contacts table. It’s similar to how the String class is called String,
 """
- class Contact
+ class Contact <ActiveRecord::Base
 
 end
